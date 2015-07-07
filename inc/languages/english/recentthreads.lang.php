32471c3f3b28c1012de858e63dd117dc778a35da
dragonexpert <arceus@teamdimensional.net>
2015-07-07 09:21:07 -0400

    Create recentthreads.lang.php

---
+++ inc/languages/english/recentthreads.lang.php
@@ -0,0 +1,11 @@
+<?php
+if(!defined("IN_MYBB"))
+{
+  die("Direct access not allowed.");
+}
+$l['recentthreads_recentthreads'] = "Recent Threads";
+$l['recentthreads_thread'] = "Thread";
+$l['recentthreads_forum'] = "Forum";
+$l['recentthreads_author'] = "Author";
+$l['recentthreads_posts'] = "Posts";
+$l['recentthreads_last_post'] = "Last Post";
