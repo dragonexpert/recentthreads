9dc50872d7b2cbd32e038f85e0cb1da6706ce6b4
dragonexpert <arceus@teamdimensional.net>
2016-08-29 07:56:46 -0500

    Fixes #8 Thread Prefixes Formatted.
    Also edited template to have forum link clickable.

--- inc/plugins/recentthread.php
+++ inc/plugins/recentthread.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 /* Hooks */
 
 $plugins->add_hook("index_end", "recentthread_list_threads");
@@ -9,14 +9,14 @@ $plugins->add_hook("xmlhttp", "recentthread_refresh_threads");
 
 function recentthread_info()
 {
-	return array(
-		"name"		=> "Recent Threads",
-		"description"		=> "A plug-in that shows the most recent threads on the index.",
-		"author"		=> "Mark Janssen",
-		"version"		=> "10.0",
-		"codename" 			=> "recentthreads",
-		"compatibility"	=> "18*"
-		);
+    return array(
+        "name"		=> "Recent Threads",
+        "description"		=> "A plug-in that shows the most recent threads on the index.",
+        "author"		=> "Mark Janssen",
+        "version"		=> "11.0",
+        "codename" 			=> "recentthreads",
+        "compatibility"	=> "18*"
+    );
 }
 
 function recentthread_install()
@@ -31,83 +31,93 @@ function recentthread_install()
 
     // Add some settings
     $new_setting_group = array(
-    "name" => "recentthreads",
-    "title" => "Recent Threads Settings",
-    "description" => "Customize various aspects of recent threads",
-    "disporder" => 77,
-    "isdefault" => 0
+        "name" => "recentthreads",
+        "title" => "Recent Threads Settings",
+        "description" => "Customize various aspects of recent threads",
+        "disporder" => 77,
+        "isdefault" => 0
     );
 
     $gid = $db->insert_query("settinggroups", $new_setting_group);
 
     $new_setting[] = array(
-    "name" => "recentthread_threadcount",
-    "title" => "Number of Threads",
-    "description" => "How many threads are shown.",
-    "optionscode" => "numeric",
-    "disporder" => 1,
-    "value" => 15,
-    "gid" => $gid
+        "name" => "recentthread_threadcount",
+        "title" => "Number of Threads",
+        "description" => "How many threads are shown.",
+        "optionscode" => "numeric",
+        "disporder" => 1,
+        "value" => 15,
+        "gid" => $gid
     );
 
     $new_setting[] = array(
-    "name" => "recentthread_threadavatar",
-    "title" => $db->escape_string("Show thread starter's avatar"),
-    "description" => $db->escape_string("If set to yes, the thread starter's avatar will be shown."),
-    "optionscode" => "yesno",
-    "disporder" => 2,
-    "value" => 0,
-    "gid" => $gid
+        "name" => "recentthread_threadavatar",
+        "title" => $db->escape_string("Show thread starter's avatar"),
+        "description" => $db->escape_string("If set to yes, the thread starter's avatar will be shown."),
+        "optionscode" => "yesno",
+        "disporder" => 2,
+        "value" => 0,
+        "gid" => $gid
     );
 
     $new_setting[] = array(
-    "name" => "recentthread_lastavatar",
-    "title" => $db->escape_string("Show last poster's avatar"),
-    "description" => $db->escape_string("If set to yes, the last poster's avatar will be shown."),
-    "optionscode" => "yesno",
-    "disporder" => 3,
-    "value" => 0,
-    "gid" => $gid
+        "name" => "recentthread_lastavatar",
+        "title" => $db->escape_string("Show last poster's avatar"),
+        "description" => $db->escape_string("If set to yes, the last poster's avatar will be shown."),
+        "optionscode" => "yesno",
+        "disporder" => 3,
+        "value" => 0,
+        "gid" => $gid
     );
 
     $new_setting[] = array(
-    "name" => "recentthread_forumskip",
-    "title" => "Forums To Ignore",
-    "description" => "The forums threads should not be pulled from.",
-    "optionscode" => "forumselect",
-    "disporder" => 4,
-    "value" => "",
-    "gid" => $gid
+        "name" => "recentthread_forumskip",
+        "title" => "Forums To Ignore",
+        "description" => "The forums threads should not be pulled from.",
+        "optionscode" => "forumselect",
+        "disporder" => 4,
+        "value" => "",
+        "gid" => $gid
     );
 
     $new_setting[] = array(
-    "name" => "recentthread_subject_length",
-    "title" => "Max Title Length",
-    "description" => "The amount of characters before the rest of the title is truncated. Enter 0 for no limit.",
-    "optionscode" => "numeric",
-    "disporder" => 5,
-    "value" => 0,
-    "gid" => $gid
+        "name" => "recentthread_subject_length",
+        "title" => "Max Title Length",
+        "description" => "The amount of characters before the rest of the title is truncated. Enter 0 for no limit.",
+        "optionscode" => "numeric",
+        "disporder" => 5,
+        "value" => 0,
+        "gid" => $gid
     );
 
     $new_setting[] = array(
-    "name" => "recentthread_subject_breaker",
-    "title" => "Word Breaking",
-    "description" => "If selected, the title will be kept to full words only in cut off.",
-    "optionscode" => "yesno",
-    "disporder" => 6,
-    "value" => 0,
-    "gid" => $gid
+        "name" => "recentthread_subject_breaker",
+        "title" => "Word Breaking",
+        "description" => "If selected, the title will be kept to full words only in cut off.",
+        "optionscode" => "yesno",
+        "disporder" => 6,
+        "value" => 0,
+        "gid" => $gid
     );
 
     $new_setting[] = array(
-    "name" => "recentthread_which_groups",
-    "title" => "Permissions",
-    "description" => "These groups cannot view the reccent threads on index.",
-    "optionscode" => "groupselect",
-    "disporder" => 7,
-    "value" => 7,
-    "gid" => $gid
+        "name" => "recentthread_which_groups",
+        "title" => "Permissions",
+        "description" => "These groups cannot view the reccent threads on index.",
+        "optionscode" => "groupselect",
+        "disporder" => 7,
+        "value" => 7,
+        "gid" => $gid
+    );
+
+    $new_setting[] = array(
+        "name" => "recentthread_prefix",
+        "title" => "Thread Prefix",
+        "description" => "If set to yes, thread prefixes will be shown.",
+        "optionscode" => "yesno",
+        "disporder" => 8,
+        "value" => 1,
+        "gid" => $gid
     );
 
     $db->insert_query_multiple("settings", $new_setting);
@@ -152,8 +162,8 @@ function recentthread_activate()
     </div>';
 
     $new_template['recentthread_thread'] = '<tr>
-    <td class="{$trow}"><a href="{$threadlink}">{$thread[\'subject\']}</a><br />{$thread[\'author\']}<br />{$posteravatar}</td>
-    <td class="{$trow}">{$thread[\'forum\']}</td>
+    <td class="{$trow}">{$recentprefix}<a href="{$threadlink}">{$thread[\'subject\']}</a><br />{$thread[\'author\']}<br />{$posteravatar}</td>
+    <td class="{$trow}"><a href="{$mybb->settings[\'bburl\']}/forumdisplay.php?fid={$thread[\'fid\']}">{$thread[\'forum\']}</a></td>
     <td class="{$trow}"><a href="javascript:MyBB.whoPosted({$thread[\'tid\']});">{$thread[\'replies\']}</a></td>
     <td class="{$trow}">{$lastposttimeago}<br />
     <a href="{$lastpostlink}">Last Post:</a> {$lastposterlink}<br />{$lastavatar}</td>
@@ -195,10 +205,10 @@ function recentthread_activate()
   </script>';
 
     foreach($new_template as $title => $template)
-	{
-		$new_template = array('title' => $db->escape_string($title), 'template' => $db->escape_string($template), 'sid' => '-1', 'version' => '1800', 'dateline' => TIME_NOW);
-		$db->insert_query('templates', $new_template);
-	}
+    {
+        $new_template = array('title' => $db->escape_string($title), 'template' => $db->escape_string($template), 'sid' => '-1', 'version' => '1800', 'dateline' => TIME_NOW);
+        $db->insert_query('templates', $new_template);
+    }
 
     require_once MYBB_ROOT . "/inc/adminfunctions_templates.php";
 
@@ -233,49 +243,49 @@ function recentthread_uninstall()
 
 function recentthread_list_threads($return=false)
 {
-	global $mybb, $db, $templates, $recentthreadtable, $recentthreads, $settings, $canviewrecentthreads, $cache, $theme, $lang;
+    global $mybb, $db, $templates, $recentthreadtable, $recentthreads, $settings, $canviewrecentthreads, $cache, $theme, $lang;
     // First check permissions
     if(!recentthread_can_view())
     {
         return;
     }
     $lang->load("recentthreads");
-	require_once MYBB_ROOT."inc/functions_search.php";
+    require_once MYBB_ROOT."inc/functions_search.php";
     $threadlimit = (int) $mybb->settings['recentthread_threadcount'];
     if(!$threadlimit) // Provide a fallback
     {
-	    $threadlimit = 15;
+        $threadlimit = 15;
     }
-	$onlyusfids = array();
-	$onlycanview = array();
-	// Check group permissions if we can't view threads not started by us
-	$group_permissions = forum_permissions();
-	foreach($group_permissions as $fid => $forum_permissions)
-	{
-		if($forum_permissions['canonlyviewownthreads'] == 1)
-		{
-			$onlyusfids[] = $fid;
-		}
-		if ($forum_permissions['canview'] == 0)
-		{
-			$onlycanview[] = $fid;
-		}
-	}
-	if(!empty($onlyusfids))
-	{
-		$where .= "AND ((t.fid IN(".implode(',', $onlyusfids).") AND t.uid='{$mybb->user['uid']}') OR t.fid NOT IN(".implode(',', $onlyusfids)."))";
-	}
-	if (!empty($onlycanview))
-	{
-		$where .= "AND (t.fid NOT IN(".implode(',', $onlycanview)."))";
-	}
-	$approved = 0;
-	
-	// Moderators can view unapproved threads
-	if($mybb->usergroup['canmodcp']==1) {
-		$approved = -1;
-	}
-	$unsearchableforums = get_unsearchable_forums();
+    $onlyusfids = array();
+    $onlycanview = array();
+    // Check group permissions if we can't view threads not started by us
+    $group_permissions = forum_permissions();
+    foreach($group_permissions as $fid => $forum_permissions)
+    {
+        if($forum_permissions['canonlyviewownthreads'] == 1)
+        {
+            $onlyusfids[] = $fid;
+        }
+        if ($forum_permissions['canview'] == 0)
+        {
+            $onlycanview[] = $fid;
+        }
+    }
+    if(!empty($onlyusfids))
+    {
+        $where .= "AND ((t.fid IN(".implode(',', $onlyusfids).") AND t.uid='{$mybb->user['uid']}') OR t.fid NOT IN(".implode(',', $onlyusfids)."))";
+    }
+    if (!empty($onlycanview))
+    {
+        $where .= "AND (t.fid NOT IN(".implode(',', $onlycanview)."))";
+    }
+    $approved = 0;
+
+    // Moderators can view unapproved threads
+    if($mybb->usergroup['canmodcp']==1) {
+        $approved = -1;
+    }
+    $unsearchableforums = get_unsearchable_forums();
     $unviewableforums = get_unviewable_forums();
     if($unsearchableforums && $unviewableforums)
     {
@@ -289,7 +299,9 @@ function recentthread_list_threads($return=false)
         $ignoreforums = " AND t.fid NOT IN(" . $mybb->settings['recentthread_forumskip'] . ") ";
     }
     $forums = $cache->read("forums");
-	$query = $db->query("
+    $prefixes = $cache->read("threadprefixes");
+
+    $query = $db->query("
 			SELECT t.*, u.username AS userusername, u.usergroup, u.displaygroup, u.avatar as threadavatar, u.avatardimensions as threaddimensions, lp.usergroup AS lastusergroup, lp.avatar as lastavatar, lp.avatardimensions as lastdimensions, lp.displaygroup as lastdisplaygroup
 			FROM ".TABLE_PREFIX."threads t
 			LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid=t.uid)
@@ -298,97 +310,101 @@ function recentthread_list_threads($return=false)
 			ORDER BY t.lastpost DESC
 			LIMIT $threadlimit
 		");
-		while($thread = $db->fetch_array($query))
-		{
-            $trow = alt_trow();
-            $thread['forum'] = $forums[$thread['fid']]['name'];
-            $threadlink = get_thread_link($thread['tid'], "", "newpost");
-            $lastpostlink = get_thread_link($thread['tid'], "", "lastpost");
-			$lastpostdate = my_date($mybb->settings['dateformat'], $thread['lastpost']);
-			$lastposttime = my_date($mybb->settings['timeformat'], $thread['lastpost']);
-            $lastposttimeago = my_date("relative", $thread['lastpost']);
-			$lastposter = $thread['lastposter'];
-			$lastposteruid = $thread['lastposteruid'];
-			$thread['author'] = build_profile_link(format_name($thread['userusername'], $thread['usergroup'], $thread['displaygroup']), $thread['uid']);
-			// Don't link to guest's profiles (they have no profile).
-			if($lastposteruid == 0)
-			{
-				$lastposterlink = $lastposter;
-			}
-			else
-			{
-				$lastposterlink = build_profile_link(format_name($lastposter, $thread['lastusergroup'], $thread['lastdisplaygroup']), $lastposteruid);
-			}
-            if($mybb->settings['recentthread_threadavatar'])
-            {
-                $threadavatar = format_avatar($thread['threadavatar'], $thread['threaddimensions']);
-                $avatarurl = $threadavatar['image'];
-                $dimensions = $threadavatar['width_height'];
-                eval("\$posteravatar = \"".$templates->get("recentthread_avatar")."\";");
-            }
-            if($mybb->settings['recentthread_lastavatar'])
-            {
-                $lastposteravatar = format_avatar($thread['lastavatar'], $thread['lastdimensions']);
-                $avatarurl = $lastposteravatar['image'];
-                $dimensions = $lastposteravatar['width_height'];
-                eval("\$lastavatar = \"".$templates->get("recentthread_avatar")."\";");
-            }
-            // Now check the length of subjects
-            $length = (int) $mybb->settings['recentthread_subject_length'];
-            if(strlen($thread['subject']) > $length && $length != 0)
-            {
-                // Figure out if we need to split it up.
-                $title = my_substr($thread['subject'], 0, $length);
-                if($mybb->settings['recentthread_subject_breaker'])
-                {
-                    $words = explode(" ", $title);
-                    $count = count($words) -1;
-                    $currenttitle = "";
-                    for($x = 0; $x < $count; $x++)
-                    {
-                        $currenttitle .= $words[$x] . " ";
-                    }
-                    $thread['subject'] = $currenttitle . " ...";
-                }
-                if(!$mybb->settings['recentthread_subject_breaker'])
-                {
-                    $thread['subject'] = $title . "...";
-                }
-            }
-		$thread['subject'] = htmlspecialchars_uni($thread['subject']);
-            // Moderator stuff baby!
-            if(is_moderator($thread['fid']))
-            {
-                $ismod = TRUE;
-                // fetch the inline mod column
-            }
-            else
-            {
-                $ismod = FALSE;
-            }
-            if(is_moderator($thread['fid'], "caneditposts") || $fpermissions['caneditposts'] == 1)
-            {
-	            $can_edit_titles = 1;
-            }
-            else
-            {
-	            $can_edit_titles = 0;
-            }
-            $inline_edit_class = '';
-		    if(($thread['uid'] == $mybb->user['uid'] && $thread['closed'] != 1 && $mybb->user['uid'] != 0 && $can_edit_titles == 1) || $ismod == true)
-		    {
-			    $inline_edit_class = "subject_editable";
-		    }
-
-			eval("\$recentthreads .= \"".$templates->get("recentthread_thread")."\";");
-            unset($posteravatar);
-            unset($lastavatar);
-		}
-        eval("\$recentthreadtable = \"".$templates->get("recentthread")."\";");
-        if($return)
+    while($thread = $db->fetch_array($query))
+    {
+        $trow = alt_trow();
+        $thread['forum'] = $forums[$thread['fid']]['name'];
+        if($mybb->settings['recentthread_prefix'])
         {
-            return $recentthreadtable;
+            $recentprefix = $prefixes[$thread['prefix']]['displaystyle'];
         }
+        $threadlink = get_thread_link($thread['tid'], "", "newpost");
+        $lastpostlink = get_thread_link($thread['tid'], "", "lastpost");
+        $lastpostdate = my_date($mybb->settings['dateformat'], $thread['lastpost']);
+        $lastposttime = my_date($mybb->settings['timeformat'], $thread['lastpost']);
+        $lastposttimeago = my_date("relative", $thread['lastpost']);
+        $lastposter = $thread['lastposter'];
+        $lastposteruid = $thread['lastposteruid'];
+        $thread['author'] = build_profile_link(format_name($thread['userusername'], $thread['usergroup'], $thread['displaygroup']), $thread['uid']);
+        // Don't link to guest's profiles (they have no profile).
+        if($lastposteruid == 0)
+        {
+            $lastposterlink = $lastposter;
+        }
+        else
+        {
+            $lastposterlink = build_profile_link(format_name($lastposter, $thread['lastusergroup'], $thread['lastdisplaygroup']), $lastposteruid);
+        }
+        if($mybb->settings['recentthread_threadavatar'])
+        {
+            $threadavatar = format_avatar($thread['threadavatar'], $thread['threaddimensions']);
+            $avatarurl = $threadavatar['image'];
+            $dimensions = $threadavatar['width_height'];
+            eval("\$posteravatar = \"".$templates->get("recentthread_avatar")."\";");
+        }
+        if($mybb->settings['recentthread_lastavatar'])
+        {
+            $lastposteravatar = format_avatar($thread['lastavatar'], $thread['lastdimensions']);
+            $avatarurl = $lastposteravatar['image'];
+            $dimensions = $lastposteravatar['width_height'];
+            eval("\$lastavatar = \"".$templates->get("recentthread_avatar")."\";");
+        }
+        // Now check the length of subjects
+        $length = (int) $mybb->settings['recentthread_subject_length'];
+        if(strlen($thread['subject']) > $length && $length != 0)
+        {
+            // Figure out if we need to split it up.
+            $title = my_substr($thread['subject'], 0, $length);
+            if($mybb->settings['recentthread_subject_breaker'])
+            {
+                $words = explode(" ", $title);
+                $count = count($words) -1;
+                $currenttitle = "";
+                for($x = 0; $x < $count; $x++)
+                {
+                    $currenttitle .= $words[$x] . " ";
+                }
+                $thread['subject'] = $currenttitle . " ...";
+            }
+            if(!$mybb->settings['recentthread_subject_breaker'])
+            {
+                $thread['subject'] = $title . "...";
+            }
+        }
+        $thread['subject'] = htmlspecialchars_uni($thread['subject']);
+        // Moderator stuff baby!
+        if(is_moderator($thread['fid']))
+        {
+            $ismod = TRUE;
+            // fetch the inline mod column
+        }
+        else
+        {
+            $ismod = FALSE;
+        }
+        if(is_moderator($thread['fid'], "caneditposts") || $fpermissions['caneditposts'] == 1)
+        {
+            $can_edit_titles = 1;
+        }
+        else
+        {
+            $can_edit_titles = 0;
+        }
+        $inline_edit_class = '';
+        if(($thread['uid'] == $mybb->user['uid'] && $thread['closed'] != 1 && $mybb->user['uid'] != 0 && $can_edit_titles == 1) || $ismod == true)
+        {
+            $inline_edit_class = "subject_editable";
+        }
+
+        eval("\$recentthreads .= \"".$templates->get("recentthread_thread")."\";");
+        unset($posteravatar);
+        unset($lastavatar);
+    }
+    eval("\$recentthreadtable = \"".$templates->get("recentthread")."\";");
+    if($return)
+    {
+        return $recentthreadtable;
+    }
 }
 
 function recentthread_get_templates()
@@ -446,7 +462,7 @@ function recentthread_can_view()
     }
     else
     {
-         return TRUE;
+        return TRUE;
     }
 }
 
