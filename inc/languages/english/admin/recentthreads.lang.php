8ac57c37dd4d7f83c38adae4422f603a4456123f
dragonexpert <arceus@teamdimensional.net>
2017-02-18 11:59:52 -0600

    Create recentthreads.lang.php

---
+++ inc/languages/english/admin/recentthreads.lang.php
@@ -0,0 +1,4 @@
+<?php
+$l['recentthreads_template'] = "Recent Thread";
+$l['recentthreads'] = "Recent Thread";
+$l['admin_log_config_plugins_update_recentthreads'] = "Updated Plugin Recent Threads On Index";
