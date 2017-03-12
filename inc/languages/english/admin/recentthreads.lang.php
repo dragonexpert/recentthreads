4b8fbd6b977d7d5a73d5b833081831ed5eaceb3d
dragonexpert <arceus@teamdimensional.net>
2017-03-12 12:47:14 -0500

    Update recentthreads.lang.php

--- inc/languages/english/admin/recentthreads.lang.php
+++ inc/languages/english/admin/recentthreads.lang.php
@@ -1,4 +1,27 @@
 <?php
+$l['setting_group_recentthreads'] = "Recent Threads Settings";
+$l['setting_group_recentthreads_desc'] = "Customize the features of Recent Threads On Index.";
+$l['setting_recentthread_threadcount'] = "Number of Threads";
+$l['setting_recentthread_threadcount_desc'] = "How many threads are shown.";
+$l['setting_recentthread_threadavatar'] = "Show thread starters avatar";
+$l['setting_recentthread_threadavatar_desc'] = "If set to yes, the thread starter's avatar will be shown.";
+$l['setting_recentthread_lastavatar'] = "Show last poster's avatar";
+$l['setting_recentthread_lastavatar_desc'] = "If set to yes, the last poster's avatar will be shown.";
+$l['setting_recentthread_forumskip'] = "Forums To Ignore";
+$l['setting_recentthread_forumskip_desc'] = "Threads from these forums will not be shown.";
+$l['setting_recentthread_subject_length'] = "Max Length of Thread Titles";
+$l['setting_recentthread_subject_length_desc'] = "The amount of characters before the rest of the title is truncated. Enter 0 for no limit.";
+$l['setting_recentthread_subject_breaker'] = "Word Breaking";
+$l['setting_recentthread_subject_breaker_desc'] = "If selected, the title will be kept to full words only in cut off.";
+$l['setting_recentthread_which_groups'] = "Permissions";
+$l['setting_recentthread_which_groups_desc'] = "These groups cannot view the reccent threads on index.";
+$l['setting_recentthread_prefix'] = "Show Prefixes?";
+$l['setting_recentthread_prefix_desc'] = "If set to yes, thread prefixes will be shown.";
+$l['setting_recentthread_prefix_only'] = "Which Prefixes?";
+$l['setting_recentthread_prefix_only_desc'] = "A thread must have one of these prefix ids to show, separate with a comma. Leave blank to not restrict.";
+$l['setting_recentthread_xthreads'] = "XThreads";
+$l['setting_recentthread_xthreads_desc'] = "If set to yes, custom thread fields will be loaded.";
 $l['recentthreads_template'] = "Recent Thread";
-$l['recentthreads'] = "Recent Thread";
+$l['recentthreads'] = "Recent Threads";
+$l['recentthreads_desc'] = "A plug-in that shows the most recent threads on the index. <a href='{2}'>Run Update Script</a><br /><a href='{1}'>Donate to support</a>";
 $l['admin_log_config_plugins_update_recentthreads'] = "Updated Plugin Recent Threads On Index";
