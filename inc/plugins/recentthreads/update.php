eda52ae4192ed5f0fa6540d1954e613c43c6a5a2
dragonexpert <arceus@teamdimensional.net>
2018-01-31 12:41:55 -0600

    Update update.php

--- inc/plugins/recentthreads/update.php
+++ inc/plugins/recentthreads/update.php
@@ -12,36 +12,59 @@ function recentthread_update()
         return;
     }
     $lang->load("recentthreads");
-    $query = $db->simple_select("settings", "*", "name IN('recentthread_show_create_date', 'recentthread_format_names')");
-    if($db->num_rows($query) == 0)
+    $have_already = array();
+    $query = $db->simple_select("settings", "*", "name IN('recentthread_show_create_date', 'recentthread_format_names', 'recentthread_pages_shown)");
+    while($old_setting = $db->fetch_array($query))
+    {
+        $have_already[] = $old_setting['name'];
+    }
+    if(count($have_already) != 3)
     {
         $q2 = $db->simple_select("settinggroups", "*", "name='recentthreads'");
         $settinggroup = $db->fetch_array($q2);
         $gid = $settinggroup['gid'];
-        $new_setting[] = array(
-            "name" => "recentthread_show_create_date",
-            "title" => "Show Creation Time",
-            "description" => "If set to yes, the thread start date will be shown.",
-            "optionscode" => "yesno",
-            "disporder" => 11,
-            "value" => 1,
-            "gid" => $gid
-        );
-
-        $new_setting[] = array(
-            "name" => "recentthread_format_names",
-            "title" => "Format Usernames",
-            "description" => "If set to yes, format the username of the thread starter and last poster.",
-            "optionscode" => "yesno",
-            "disporder" => 12,
-            "value" => 1,
-            "gid" => $gid
-        );
+        if (!in_array("recentthread_show_create_date", $have_already))
+        {
+            $new_setting[] = array(
+                "name" => "recentthread_show_create_date",
+                "title" => "Show Creation Time",
+                "description" => "If set to yes, the thread start date will be shown.",
+                "optionscode" => "yesno",
+                "disporder" => 11,
+                "value" => 1,
+                "gid" => $gid
+            );
+        }
+        if (!in_array("recentthread_format_names", $have_already))
+        {
+            $new_setting[] = array(
+                "name" => "recentthread_format_names",
+                "title" => "Format Usernames",
+                "description" => "If set to yes, format the username of the thread starter and last poster.",
+                "optionscode" => "yesno",
+                "disporder" => 12,
+                "value" => 1,
+                "gid" => $gid
+            );
+        }
+        if (!in_array("recentthread_pages_shown", $have_already))
+        {
+            $new_setting = array(
+                "name" => "recentthread_pages_shown",
+                "title" => "Show On These Pages",
+                "description" => "The pages to show the recent threads on.  One entry per line.",
+                "optionscode" => "textarea",
+                "disporder" => 13,
+                "value" => "index.php",
+                "gid" => $gid
+            );
+        }
 
         $db->insert_query_multiple("settings", $new_setting);
         rebuild_settings();
         flash_message($lang->admin_log_config_plugins_update_recentthreads, "success");
         admin_redirect("index.php?module=config-plugins");
     }
+    flash_message($lang->recentthread_current, "error");
     admin_redirect("index.php?module=config-plugins");
 }
