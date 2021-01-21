6816b946cbfe7a5cb504149685a91ee6dff71508
dragonexpert <arceus@teamdimensional.net>
2021-01-20 18:39:50 -0600

    Update recentthreads.lang.php

--- inc/languages/english/admin/recentthreads.lang.php
+++ inc/languages/english/admin/recentthreads.lang.php
@@ -27,3 +27,4 @@ $l['recentthreads_desc'] = "A plug-in that shows the most recent threads on the 
 $l['admin_log_config_plugins_update_recentthreads'] = "Updated Plugin Recent Threads On Index";
 $l['recentthread_cant_install'] = "This plugin requires version 1.8.1 or higher.";
 $l['recentthread_current'] = "You have the most current version of this plugin.";
+$l['recentthreads_can_template'] = "Can Use Recent Threads Template Tool?";
